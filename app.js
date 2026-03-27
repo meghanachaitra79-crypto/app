54ba539bedd4f492929c6b16e90cea7c49c3335b
meghanachaitra79-crypto <meghanachaitra79@gmail.com>
2026-03-27 14:16:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+{
+  "type": "Compass Connections",
+  "version": {
+    "$numberInt": "1"
+  },
+  "connections": [
+    {
+      "id": "cb9a4044-5c18-4283-b3e3-2a1fefadeba5",
+      "lastUsed": {
+        "$date": {
+          "$numberLong": "1774593409001"
+        }
+      },
+      "favorite": {
+        "name": "Meghana R",
+        "color": "color5"
+      },
+      "savedConnectionType": "favorite",
+      "connectionOptions": {
+        "connectionString": "mongodb://localhost:27017/",
+        "oidc": {}
+      }
+    }
+  ]
+}
